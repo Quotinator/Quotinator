6129aa807bfa61237996939e469979c8d3474c11
Travis Beck <tjbenator@gmail.com>
2014-08-13 03:47:39 +0000

    Forgot to save a couple files

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -84,15 +84,5 @@ class HomeController extends BaseController
 		return View::make('quote')->with('quote', Quote::find($id));
 	}
 
-	public function getUserQuotes(User $username) {
-		$quotes = $username->quotes()->orderBy('id', 'desc')->paginate(Config::get('per_page'));
-		$quoteCount = $quotes->count();
-		return View::make('user.quotes')->with('user', $username)->with('quotes', $quotes)->with('count', $quoteCount);
-	}
 
-	public function getFavorites(User $username) {
-		$quotes = $username->favorites()->orderBy('id', 'desc')->paginate(Config::get('per_page'));
-		$quotesCount = $quotes->count();
-		return View::make('user.favorites')->with('user', $username)->with('quotes', $quotes)->with('count', $quotesCount);
-	}
 }
--- app/models/Vote.php
+++ app/models/Vote.php
@@ -2,7 +2,13 @@
 
 class Vote extends Eloquent {
 
+	public function user() {
+		return $this->belongsTo('User');
+	}
 
+	public function quote() {
+		return $this->belongsTo('Quote');
+	}
 	public function user() {
 		return $this->belongsTo('User');
 	}
