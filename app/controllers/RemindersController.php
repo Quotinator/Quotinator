103113b2eb973a9ba1f0340c5a2320bc5ca03169
Travis Beck <tjbenator@gmail.com>
2014-08-31 20:02:11 +0000

    Minor style change. Added Subject to password email reminders

--- app/controllers/RemindersController.php
+++ app/controllers/RemindersController.php
@@ -19,7 +19,12 @@ class RemindersController extends Controller {
 	 */
 	public function postRemind()
 	{
-		switch ($response = Password::remind(Input::only('email')))
+		$response = Password::remind(Input::only('email'), function($message){
+				$message->subject(trans('messages.password_reset')); 
+		});
+
+
+		switch ($response)
 		{
 			case Password::INVALID_USER:
 				return Redirect::back()->with('error', Lang::get($response));
--- app/lang/en/messages.php
+++ app/lang/en/messages.php
@@ -2,5 +2,6 @@
 return array(
 	'userquotes' => '{0} :username is lame and hasn\' submitted any quotes!| [1,1] :username submitted :count quote!|[2,Inf] :username has submitted :count quotes!',
 	'userfavorites' => '{0} :username is lame and doesn\'t have any favorites!| [1,1] :username has :count favorite!|[2,Inf] :username has :count favorites!',
-	'search' => '{0} No results found for ":terms"!| [1,1] Only a single quote for ":terms"!|[2,Inf] :count results for ":terms"!'
+	'search' => '{0} No results found for ":terms"!| [1,1] Only a single quote for ":terms"!|[2,Inf] :count results for ":terms"!',
+	'password_reset' => 'Password Reset Link',
 	);
