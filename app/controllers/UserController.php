3bf90455621cb1aedb17ff3fd594383628d175d3
Travis Beck <tjbenator@gmail.com>
2014-09-07 16:59:34 +0000

    Fixed quote counts on user pages. Closes #7.
    Fixed wrong quote counts on both User Favorites and User Quotes pages.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,21 +4,29 @@ class UserController extends PageController
 {
 	public function getProfile(User $user) {
 		$this->layout->title = $user->username . "'s profile";
+		
 		$quotes = $user->quotes()->orderBy('id', 'desc')->whereStatus(1)->take(3);
+		
 		$this->layout->nest('content', 'user.profile', ['user' => $user, 'quotes' => $quotes]);
 	}
 	
 	public function getQuotes(User $user) {
 		$this->layout->title = $user->username . "'s submitted quotes";
-		$quotes = $user->quotes()->orderBy('id', 'desc')->whereStatus(1)->paginate(Config::get('per_page'));
+		
+		$quotes = $user->quotes()->orderBy('id', 'desc')->whereStatus(1);
 		$quoteCount = $quotes->count();
-		$this->layout->nest('content', 'user.quotes', ['user' => $user, 'quotes' => $quotes, 'count' => $quoteCount]);
+		$paginated = $quotes->paginate(Config::get('per_page'));
+
+		$this->layout->nest('content', 'user.quotes', ['user' => $user, 'quotes' => $paginated, 'count' => $quoteCount]);
 	}
 
 	public function getFavorites(User $user) {
 		$this->layout->title = $user->username . "'s favorites";
-		$quotes = $user->favorites()->orderBy('id', 'desc')->whereStatus(1)->paginate(Config::get('per_page'));
+		
+		$quotes = $user->favorites()->orderBy('quote_id', 'desc')->whereStatus(1);
 		$quoteCount = $quotes->count();
-		$this->layout->nest('content', 'user.favorites', ['user' => $user, 'quotes' => $quotes, 'count' => $quoteCount]);	
+		$paginated = $quotes->paginate(Config::get('per_page'));
+		
+		$this->layout->nest('content', 'user.favorites', ['user' => $user, 'quotes' => $paginated, 'count' => $quoteCount]);
 	}
 }
