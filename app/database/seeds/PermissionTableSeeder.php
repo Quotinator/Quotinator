18ed563a97ec346ab85fb3d0e019d596e5ca2d94
Travis Beck <tjbenator@gmail.com>
2015-01-17 14:02:51 -0800

    Updated permissions. Added UpdateSeeder.

--- app/database/seeds/PermissionTableSeeder.php
+++ app/database/seeds/PermissionTableSeeder.php
@@ -3,22 +3,48 @@ class PermissionTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::table('permissions')->delete();
+        $roles = [
+            ['name' => 'Administrator', 'weight' => 3],
+            ['name' => 'Moderator', 'weight' => 2],
+            ['name' => 'User', 'weight' => 1],
+        ];
+        $permissions = [
+            ['weight' =>  '1', 'node' => 'quote.editor',      'description' => 'The ability to use the editor'],
+            ['weight' =>  '1', 'node' => 'quote.edit',        'description' => 'The ability to edit an existing quote'],
+            ['weight' =>  '2', 'node' => 'quote.edit.others', 'description' => 'The ability to edit an existing quote that isn\'t yours'],
+            ['weight' =>  '1', 'node' => 'quote.new',         'description' => 'The ability to create a new quote'],
+            ['weight' =>  '2', 'node' => 'quote.approve',     'description' => 'The ability to approve a quote'],
+            ['weight' =>  '2', 'node' => 'quote.deny',        'description' => 'The ability to deny a quote'],
+            ['weight' =>  '1', 'node' => 'quote.upvote',      'description' => 'The ability to upvote a quote'],
+            ['weight' =>  '1', 'node' => 'quote.downvote',    'description' => 'The ability to downvote a quote'],
+            ['weight' =>  '1', 'node' => 'quote.favorite',    'description' => 'The ability to create favorites'],
+            ['weight' =>  '1', 'node' => 'user.preferences',  'description' => 'The ability to access user preferences'],
+            ['weight' =>  '3', 'node' => 'site.preferences',  'description' => 'The ability to access site preferences'],
+        ];
 
-        $permissions[]['node'] = 'quote.edit';
-        $permissions[]['node'] = 'quote.submit';
-        $permissions[]['node'] = 'quote.approve';
-        $permissions[]['node'] = 'quote.deny';
-        $permissions[]['node'] = 'quote.favorite';
-        $permissions[]['node'] = 'user.preferences';
-        $permissions[]['node'] = 'site.preferences';
-        $role = Role::where('name', '=', 'Administrator')->first();
-        foreach ($permissions as $permission) {
-            $perm = new Permission;
-            $perm->node = $permission['node'];
-            $perm->description = ucfirst(str_replace('.', ' ', $permission['node']));
+        foreach ($permissions as $permission)
+        {
+            $perm = Permission::firstOrCreate(['node' => $permission['node']]);
+            $perm->description = $permission['description'];
             $perm->save();
-            $role->permissions()->attach($perm);
+        }
+
+        foreach ($roles as $r)
+        {
+            $this->command->info('Adding permissions to ' . $r['name']);
+            $role = Role::where('name', $r['name'])->first();
+            foreach ($permissions as $p)
+            {
+                if ($p['weight'] <= $r['weight'])
+                {
+                    $this->command->info('Giving ' . $r['name'] .' the '. $p['node'] . ' node');
+                    $permission = Permission::where('node', $p['node'])->first();
+                    if (!$role->permissions->contains($permission->id))
+                    {
+                        $role->permissions()->attach($permission);
+                    }
+                }
+            }
         }
 
     }
---
+++ app/database/seeds/UpdateSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class UpdateSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		$this->call('PermissionTableSeeder');
+	}
+
+}
