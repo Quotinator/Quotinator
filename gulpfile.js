01038e8d1a2b5a60e9e1f5e185a53f45b5044307
Travis Beck <tjbenator@gmail.com>
2015-07-19 02:46:15 -0700

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,8 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+        "main.css"
+    ]);
+    mix.version("css/all.css");
 });
