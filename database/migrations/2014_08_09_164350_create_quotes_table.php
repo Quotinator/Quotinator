1281b75560aadc76e72a63065ba1142e745ba782
Travis Beck <tjbenator@gmail.com>
2015-07-19 01:31:05 -0700

    Migrations from master. New migration for table rename.

---
+++ database/migrations/2014_08_09_164350_create_quotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('quotes', function($table)
+		{
+			$table->increments('id');
+			$table->unsignedInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+			$table->string('title', 64);
+			$table->longText('quote');
+			$table->tinyInteger('status');
+			$table->float('confidence');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('quotes');
+	}
+
+}
